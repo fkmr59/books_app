8b4ead79f617eff3691ffc34f38eb6204775662c
terumitt <f4k0m6r0@icloud.com>
2023-02-15 15:30:50 +0900

    migrarionからのDB

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_055123) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_062628) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
