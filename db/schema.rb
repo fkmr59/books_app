e25725ec9d11718d602775ee95220424b3dda03f
terumitt <f4k0m6r0@icloud.com>
2023-02-16 16:09:15 +0900

    画像保存のDBテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_062628) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_070715) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
